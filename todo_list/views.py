abdddfeaa68b71478f4e89b06f8a9e51a96e7ea1
Firat Saltik <f_saltik@yahoo.com>
2020-02-16 06:51:36 +0300

    messages and delete item

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ListForm
 from .models import List
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -13,6 +15,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request, 'item has been added to list')
             return render(request, 'home.html', {'all_items': all_items})
 
     else:
@@ -22,3 +25,10 @@ def home(request):
 def about(request):
    context = {'name': 'f', 'last_name': 'saltik' }
    return render(request, 'about.html', context)
+
+def delete(request,list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, 'item has been deleted')
+    return redirect('home')
+
