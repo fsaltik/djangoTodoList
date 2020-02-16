aa4fbe6f4fc418124f40d863cde26f7a6d40bdf2
Firat Saltik <f_saltik@yahoo.com>
2020-02-16 07:26:37 +0300

    Editing items

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('about', views.about, name='about'),
     path('delete/<list_id>', views.delete, name='delete'),
+    path('cross_off/<list_id>', views.cross_off, name='cross_off'),
+    path('uncross/<list_id>', views.uncross, name='uncross'),
+    path('edit/<list_id>', views.edit, name='edit'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import ListForm
 from .models import List
 from django.contrib import messages
@@ -8,7 +8,6 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 
 def home(request):
-
     if request.method == 'POST':
         form = ListForm(request.POST or None)
 
@@ -20,15 +19,47 @@ def home(request):
 
     else:
         all_items = List.objects.all
-        return render(request, 'home.html', {'all_items' : all_items})
+        return render(request, 'home.html', {'all_items': all_items})
+
 
 def about(request):
-   context = {'name': 'f', 'last_name': 'saltik' }
-   return render(request, 'about.html', context)
+    context = {'name': 'f', 'last_name': 'saltik'}
+    return render(request, 'about.html', context)
 
-def delete(request,list_id):
+
+def delete(request, list_id):
     item = List.objects.get(pk=list_id)
     item.delete()
     messages.success(request, 'item has been deleted')
     return redirect('home')
 
+
+def cross_off(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    messages.success(request, 'item has been updated')
+    return redirect('home')
+
+
+def uncross(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    messages.success(request, 'item has been updated')
+    return redirect('home')
+
+
+def edit(request, list_id):
+    if request.method == 'POST':
+        item =List.objects.get(pk=list_id)
+        form = ListForm(request.POST or None,instance=item)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'item has been edited')
+            return redirect('home')
+
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item})
