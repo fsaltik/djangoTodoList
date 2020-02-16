88797b6f052b4c3b98f8a29b88cbeb30e141b9e2
Firat Saltik <f_saltik@yahoo.com>
2020-02-16 06:28:26 +0300

    home view database posting

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from .forms import ListForm
 from .models import List
+
 
 # Create your views here.
 
 def home(request):
-    all_items = List.objects.all
-    return render(request, 'home.html', {'all_items' : all_items})
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            return render(request, 'home.html', {'all_items': all_items})
+
+    else:
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items' : all_items})
 
 def about(request):
    context = {'name': 'f', 'last_name': 'saltik' }
